4861d2ce5c19712bafabad3c0e8ea8e820d0c3ee
Nirjan Khadka <webdev.nk@gmail.com>
2023-10-06 15:40:53 +0545

    style(linting): add more linting rules to prevent over complex code

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -20,5 +20,26 @@ module.exports = {
     'write-good-comments/write-good-comments': 'warn',
     'no-secrets/no-secrets': 'error',
     'func-names': ['error', 'as-needed'],
+    'max-lines': [
+      'warn',
+      {
+        max: 300,
+        skipBlankLines: true,
+        skipComments: true,
+      },
+    ],
+    'max-lines-per-function': [
+      'warn',
+      {
+        max: 60,
+        skipBlankLines: true,
+        skipComments: true,
+      },
+    ],
+    complexity: ['warn', 5],
+    'max-depth': ['error', 3],
+    'max-nested-callbacks': ['error', 4],
+    'max-params': ['error', 3],
+    'no-await-in-loop': 'error',
   },
 };
