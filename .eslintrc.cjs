45e5ba23fbe0d2bbd73338716d1a044b4ee85e37
Nirjan Khadka <webdev.nk@gmail.com>
2023-10-06 12:12:50 +0545

    style(linting): add more eslint plugins and rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,3 +1,23 @@
 module.exports = {
-  extends: ['@nuxtjs/eslint-config-typescript', 'prettier'],
+  extends: [
+    '@nuxtjs/eslint-config-typescript',
+    'prettier',
+    'plugin:sonarjs/recommended',
+    'plugin:vue-scoped-css/vue3-recommended',
+    'plugin:@getify/proper-arrows/getify-says',
+    'plugin:security/recommended',
+  ],
+  plugins: [
+    'sonarjs',
+    'promise',
+    'pure',
+    'no-loops',
+    'write-good-comments',
+    'no-secrets',
+  ],
+  rules: {
+    'no-loops/no-loops': 2,
+    'write-good-comments/write-good-comments': 'warn',
+    'no-secrets/no-secrets': 'error',
+  },
 };
